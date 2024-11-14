8dcd367151bf05a036552166a929485334e01b91
ivanextrastuff <19ileung@CMS.tela.org.uk>
2024-11-14 11:44:30 +0000

    Distinct power

--- distinctPower.js
+++ distinctPower.js
@@ -3,7 +3,7 @@ let temp = 0
 for (let i = 2n; i <= 100n; i++){
     for (let j = 2n; j <= 100n; j++) {
         temp = i**j
-        if (arr.indexOf(temp)===-1) arr.push(temp)
+        if (!arr.includes(temp)) arr.push(temp)
     }
 }
 console.log(arr.length)
